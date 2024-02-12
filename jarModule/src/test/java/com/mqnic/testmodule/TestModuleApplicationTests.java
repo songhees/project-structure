e39c244023c41570f66cc7a82a349c6986f78029
songhees <osh4880@gmail.com>
2024-02-12 15:23:25 +0900

    Create jar module

---
+++ jarModule/src/test/java/com/mqnic/testmodule/TestModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mqnic.testmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
