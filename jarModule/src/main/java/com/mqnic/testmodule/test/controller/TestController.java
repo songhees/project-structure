1b1e89a740f8704b4b4df347bc705284babb23ad
songheeOh <osh4880@gmail.com>
2024-02-12 16:09:32 +0900

    Fix : jar 파일 의존성 수정

--- jarModule/src/main/java/com/mqnic/testmodule/test/controller/TestController.java
+++ jarModule/src/main/java/com/mqnic/testmodule/test/controller/TestController.java
@@ -1,2 +1,17 @@
-package com.mqnic.testmodule.test.controller;public class TestController {
+package com.mqnic.testmodule.test.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/")
+public class TestController {
+
+    @GetMapping("/")
+    public String testList() {
+        return "index";
+    }
 }
